ab4e48eb7ea1f19071c6fc8c8c98cad7d6d6db13
Sabsunix <39288059+Sabsunix@users.noreply.github.com>
2018-10-23 14:52:21 -0700

    cleanup 1

--- quest8/Triangle.java
+++ quest8/Triangle.java
@@ -3,8 +3,8 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 /**
  * Write a description of class Triangle here.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Cole Tomaro
+ * @version 10/23/2018
  */
 public class Triangle extends Actor
 {
@@ -12,12 +12,13 @@ public class Triangle extends Actor
     //public int[] y1 = {49, 0, 49}; // original y coords
     public int[] x1 = {0, 49, 0}; // fixed x coords for -90 rotation
     public int[] y1 = {0, 24, 49}; // fixed y coords for -90 rotation
-    public int tMoveSpeed = 5;
+    public int tMoveSpeed = 5; // speed at which it moves
     
     public Triangle()
     {
         GreenfootImage image = new GreenfootImage(50, 50);
-        setRotation(-90);
+		
+        setRotation(-90); // spawns facing upwards
         
         image.setColor(Color.RED); // make this vary for key press?
         image.fillPolygon(x1, y1, x1.length);
@@ -31,16 +32,13 @@ public class Triangle extends Actor
      */
     public void act() 
     {
-        // Make it so that triangles move up to the top of the screen
-        
-        /*if (triangleY > 0)
-        {
-            triangleY++;
-        }*/
         tMove();
-        //System.out.println( getY() );
     }
-    
+	
+    /**
+     * tMove - move at tMoveSpeed until it hits the top of the world
+     * in which case, remove the object.
+     */
     public void tMove()
     {
         if (getY() > 0)
@@ -51,13 +49,5 @@ public class Triangle extends Actor
         {
             getWorld().removeObject(this); 
         }
-        
-        //setLocation( getX()++, getY()++ );
-        
-        /*
-         
-        */
-        
-        
     }
 }
