10ed584636bd9e74508394e8c3b888aa25eda2d4
Nathanielel <44421830+Nathanielel@users.noreply.github.com>
2018-11-16 17:17:34 -0800

    Add files via upload

---
+++ project/prototype1/Player.java
@@ -0,0 +1,30 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Player here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Player extends Sprite
+{
+    private int health;
+    private boolean shielded = false;
+    /**
+     * Act - do whatever the Player wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+    public int getHealth(){
+        return health;
+    }
+    public void setHealth(int newHealth){
+        health = newHealth;
+    }
+    public void hasShield(){
+        shielded = true;
+    }
+}
