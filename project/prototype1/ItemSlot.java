dd0935a1059f2814bec549bf39625219b5702a00
Sabsunix <39288059+Sabsunix@users.noreply.github.com>
2018-11-20 00:09:17 -0800

    fix description

--- project/prototype1/ItemSlot.java
+++ project/prototype1/ItemSlot.java
@@ -1,20 +1,16 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class ItemSlot here.
+ * Shows what item(s) the play is currently holding
  * 
  * @author Cole Tomaro
  * @version 11/19/18
  */
 public class ItemSlot extends HUD
 {
-    /**
-     * Act - do whatever the ItemSlot wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+   
     public void act() 
     {
-        // Add your action code here.
     }
     
     public void addItem()
