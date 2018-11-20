28e9b7fe27d8b968eb0c58a9ea4c0249ca96c75a
Sabsunix <39288059+Sabsunix@users.noreply.github.com>
2018-11-20 00:07:56 -0800

    fix description

--- project/prototype1/HealthBar.java
+++ project/prototype1/HealthBar.java
@@ -1,7 +1,7 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class HealthBar here.
+ * Displays the player's health
  * 
  * @author Cole Tomaro
  * @version 11/19/18
@@ -10,7 +10,6 @@ public class HealthBar extends HUD
 {
     public void act() 
     {
-        // Add your action code here.
     }
     
     public void addHealth(int amount)
