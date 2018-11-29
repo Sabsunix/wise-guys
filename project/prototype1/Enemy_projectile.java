a5ad741f8a03af55872132780f7c091df4838b85
PeasantLord <44455713+PeasantLord@users.noreply.github.com>
2018-11-29 04:21:56 -0800

    Update Enemy_projectile.java

--- project/prototype1/Enemy_projectile.java
+++ project/prototype1/Enemy_projectile.java
@@ -4,7 +4,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * The projectile the shooting enemy will shoot
  * 
  * @author William Craig
- * @version 11/20/18
+ * @version 11/29/18
  */
 public class Enemy_projectile extends Enemy
 {
@@ -12,6 +12,11 @@ public class Enemy_projectile extends Enemy
      * Act - do whatever the Enemy_projectile wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    public Enemy_projectile(int direction)
+    {
+        int direct = direction;
+        speed(direct);
+    }
     public void act() 
     {
         Actor p = getOneIntersectingObject(Platform.class);
@@ -20,10 +25,18 @@ public class Enemy_projectile extends Enemy
             World world = getWorld();
             world.removeObject(this);
         }
+        move();
+        getCollisionGeneral();
     }    
-    @Override
-    public void move()
+    public void speed(int direct)
     {
-        
+        if(direct==1)
+        {
+            setVelocityX(-16);
+        }
+        else
+        {
+            setVelocityX(16);
+        }
     }
 }
