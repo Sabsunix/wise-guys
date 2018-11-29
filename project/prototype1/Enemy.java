fae49d0defcdec76f0e50aa522f22b22ca87af93
PeasantLord <44455713+PeasantLord@users.noreply.github.com>
2018-11-29 04:22:55 -0800

    Update Enemy.java

--- project/prototype1/Enemy.java
+++ project/prototype1/Enemy.java
@@ -1,10 +1,10 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Enemy here.
+ * Superclass for all Enemies
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author William Craig
+ * @version 11/29/18
  */
 public class Enemy extends Sprite
 {
@@ -12,11 +12,20 @@ public class Enemy extends Sprite
      * Act - do whatever the Enemy wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    public void act() 
+    public void getCollisionGeneral() 
     {
         if(isTouching(Hammer.class)){
             //play death sound?
             getWorld().removeObject(this);
         }
-    }    
+        if(isTouching(Shield.class)){
+            Actor Shield = getOneIntersectingObject(Shield.class);
+            getWorld().removeObject(Shield);
+            getWorld().removeObject(this);
+        }
+        else if(isTouching(Nebukar.class)){
+            Actor Nebukar = getOneIntersectingObject(Nebukar.class);
+            getWorld().removeObject(Nebukar);
+        }
+    }
 }
