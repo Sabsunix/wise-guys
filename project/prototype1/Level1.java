3f12b10090ba13787cb2cac4dca20e028ea6828d
Nathanielel <44421830+Nathanielel@users.noreply.github.com>
2018-12-05 15:41:48 -0800

    Update Level1.java

--- project/prototype1/Level1.java
+++ project/prototype1/Level1.java
@@ -26,7 +26,7 @@ public class Level1 extends World
         super(400, 400, 1, false);
         leftX = TILE_WIDTH / 2;
         topY = TILE_HEIGHT / 2 - getHeight() % TILE_HEIGHT; 
-        setPaintOrder(Nebukar.class, Enemy.class, Platform.class, Shield.class);
+        setPaintOrder(Nebukar.class, Platform.class, Shield.class, Enemy.class);
     }
     
     /**
