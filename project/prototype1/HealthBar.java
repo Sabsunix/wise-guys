33a0b708e60ce6f8f4125ef704aec6b4bf77b8eb
Sabsunix <39288059+Sabsunix@users.noreply.github.com>
2018-12-05 23:00:40 -0800

    Finally got a dynamic health bar

--- project/prototype1/HealthBar.java
+++ project/prototype1/HealthBar.java
@@ -1,4 +1,5 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import java.util.List;
 
 /**
  * Displays the player's health
@@ -6,17 +7,30 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author Cole Tomaro
  * @version 11/19/18
  */
-public class HealthBar extends HUD
+public class HealthBar extends Actor
 {
-    public void act() 
+    public static final int HEIGHT = 25;
+    int health;
+    GreenfootImage barFill = new GreenfootImage(100 + 1, HEIGHT + 1);
+    Level1 w = (Level1) getWorld();
+    
+    public HealthBar()
     {
+        barFill.setColor(Color.RED);
+        barFill.fillRect(0, 0, 100, HEIGHT); //edit this
+        setImage(barFill);
     }
     
-    public void addHealth(int amount)
+    public void setSize(int number)
     {
+        health = number;
+        draw();
     }
     
-    public void subtractHealth(int amount)
+    public void draw()
     {
+        barFill.clear();
+        barFill.fillRect(0, 0, health, HEIGHT);
+        setImage(barFill);
     }
 }
