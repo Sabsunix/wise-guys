4713b69ff863aec30ef40af6d6133aa6bc612198
PeasantLord <44455713+PeasantLord@users.noreply.github.com>
2018-12-05 23:18:11 -0800

    Made enemy an abstract

--- project/prototype1/Enemy.java
+++ project/prototype1/Enemy.java
@@ -6,7 +6,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author William Craig
  * @version 11/29/18
  */
-public class Enemy extends Sprite
+public abstract class Enemy extends Sprite
 {
     /**
      * Act - do whatever the Enemy wants to do. This method is called whenever
